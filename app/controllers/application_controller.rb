1446493f0f82a09c52ce973c9d87e9ce149d842d
pharia-le <pharia.le@gmail.com>
2020-04-17 14:50:41 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,43 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params["id"])
+    erb :show
+  end
+
+  post '/articles' do
+    article = Article.new(params)
+    if article.save
+      redirect "/articles/#{article.id}"
+    else
+      redirect "/articles/new"
+    end
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    params.delete("_method")
+    if article.update(params)
+      redirect "/articles/#{article.id}"
+    else
+      redirect "/articles/#{article.id}/edit"
+    end
+  end
+
 end
